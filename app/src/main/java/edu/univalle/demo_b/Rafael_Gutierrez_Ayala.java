2ea06843de4c3e9037571f41b2071572bedb2009
universitario <jhimyendcon@hotmail.com>
2019-04-16 17:22:06 -0400

    TY

--- app/src/main/java/edu/univalle/demo_b/Rafael_Gutierrez_Ayala.java
+++ app/src/main/java/edu/univalle/demo_b/Rafael_Gutierrez_Ayala.java
@@ -2,5 +2,5 @@ package edu.univalle.demo_b;
 
 public class Rafael_Gutierrez_Ayala {
 
-    /*La  experiencia que he ganado, es solo mia y no de otras personas*/
+    /*La experiencia que he ganado, es solo mia y no de otras personas*/
 }
